d914a1ecc12ab71453331277b9c6512dce3ddc89
nkmr-jp <nkmr.git@gmail.com>
2023-11-23 18:40:54 +0900

    :memo: Remove comments

--- tests/helper/diff_helper.bash
+++ tests/helper/diff_helper.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# ファイルが一致するかどうかを確認する関数
 assert_files_match() {
   local generated_file="./$BASE_HOST$1"
   local fixture_file="./fixture/$BASE_HOST$1"
@@ -8,5 +7,5 @@ assert_files_match() {
   diff_output=$(diff "$generated_file" "$fixture_file")
   diff_status=$?
 
-  [ "$diff_status" -eq 0 ] # diff コマンドが0を返した場合、ファイルは一致しています
+  [ "$diff_status" -eq 0 ]
 }
